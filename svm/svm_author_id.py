7d7be432083d860f4158f605fe4ca6d277bcbbb6
mahendrakariya <mahendrakariya@gmail.com>
2015-06-28 21:47:16 +0530

    SVM assignment

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -9,12 +9,15 @@
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from time import time
+import numpy as np
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,6 +29,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
+# features_train = features_train[:len(features_train) / 100]
+# labels_train = labels_train[:len(labels_train) / 100]
+
+clf = SVC(kernel="rbf", C=10000.0)
+
+print
+print 'Now training...'
+print
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print 'Fitting done:', round(time() - t0, 3), "s"
+
+pred = clf.predict(features_test)
+unique, counts = np.unique(pred, return_counts=True)
+
+print np.asarray((unique, counts)).T
+
+print 'Prediction done:', round(time() - t0, 3), "s"
 
 #########################################################
 
