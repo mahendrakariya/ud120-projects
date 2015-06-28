ec0f9a697e23493b9ecd715ba1f3cfe363aa437e
mahendrakariya <mahendrakariya@gmail.com>
2015-06-28 12:00:51 +0530

    Naive Bayes assignment

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,7 +12,9 @@
 """
     
 import sys
+from sklearn.naive_bayes import GaussianNB
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -22,12 +24,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+print clf.score(features_test, labels_test)
 
 #########################################################
 
