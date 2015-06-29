85499948a3433326e90930645115e90c6d434edd
mahendrakariya <mahendrakariya@gmail.com>
2015-06-29 19:32:20 +0530

    Trying out kNN

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.neighbors import KNeighborsClassifier
+
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -10,21 +12,21 @@ features_train, labels_train, features_test, labels_test = makeTerrainData()
 ### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
 ### in together--separate them so we can give them different colors in the scatterplot,
 ### and visually identify them
-grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii] == 1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii] == 1]
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color="b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color="r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 #################################################################################
 
 
@@ -32,11 +34,9 @@ plt.show()
 ### visualization code (prettyPicture) to show you the decision boundary
 
 
-
-
-
-
-
+clf = KNeighborsClassifier(n_neighbors=10, weights='distance')
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
 
 try:
     prettyPicture(clf, features_test, labels_test)
