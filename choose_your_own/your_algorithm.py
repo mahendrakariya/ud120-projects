1cb73ec01572be405374ff15e5591b25fc16f869
mahendrakariya <mahendrakariya@gmail.com>
2015-06-29 19:53:19 +0530

    trying all at once

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -33,12 +37,31 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print 'GaussianNB:', clf.score(features_test, labels_test)
+
+clf = SVC()
+clf.fit(features_train, labels_train)
+print 'SVC:', clf.score(features_test, labels_test)
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print 'DecisionTreeClassifier:', clf.score(features_test, labels_test)
+
+clf = AdaBoostClassifier(n_estimators=100)
+clf.fit(features_train, labels_train)
+print 'AdaBoostClassifier:', clf.score(features_test, labels_test)
+
+clf = KNeighborsClassifier(n_neighbors=10, weights='distance')
+clf.fit(features_train, labels_train)
+print 'KNeighborsClassifier:', clf.score(features_test, labels_test)
 
 clf = RandomForestClassifier(n_estimators=1000, criterion='entropy')
 clf.fit(features_train, labels_train)
-print clf.score(features_test, labels_test)
+print 'RandomForestClassifier:', clf.score(features_test, labels_test)
 
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+# try:
+#     # prettyPicture(clf, features_test, labels_test)
+# except NameError:
+#     pass
