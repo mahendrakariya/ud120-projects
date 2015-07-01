8bf5956e26486d0c31c2cd4ed887a31706a138bd
mahendrakariya <mahendrakariya@gmail.com>
2015-07-01 16:33:59 +0530

    exploring dataset

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,11 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+# print enron_data.keys()
+# print enron_data['METTS MARK']
+# salaries_null = [p for p in enron_data.keys() if enron_data[p]['total_payments'] == 'NaN' and enron_data[p]['poi'] == True]
+# print salaries_null
+print float(len(enron_data.keys()))
+
 
 
