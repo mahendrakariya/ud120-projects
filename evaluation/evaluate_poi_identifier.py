d6ddc95168cd493000b32d9a6e791225d88f9643
mahendrakariya <mahendrakariya@gmail.com>
2015-07-03 23:00:28 +0530

    validation assignment

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -15,6 +15,9 @@
 import pickle
 import sys
 sys.path.append("../tools/")
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from feature_format import featureFormat, targetFeatureSplit
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
@@ -26,7 +29,19 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+### your code goes here
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3,
+                                                                            random_state=42)
 
-### your code goes here 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print len(pred)
+
+print accuracy_score(labels_test, pred)
+print precision_score(labels_test, pred)
+print recall_score(labels_test, pred)
+print
+print clf.score(features_test, labels_test)
 
 
